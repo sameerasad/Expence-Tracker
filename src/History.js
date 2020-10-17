3ceef326aa8023f68b91ad38d9ea7d3690a03c76
sameerasad <ssameerkhan525@gmail.com>
2020-10-17 15:32:57 +0500

    react challenge day 4 transaction data putting inside html as dynamic array of transaction

--- src/History.js
+++ src/History.js
@@ -2,22 +2,30 @@ import React from 'react'
 
 
 const History = () => {
+
+    let transaction = [{
+        amount:  500, desc: "Cash",
+        amount: -200, desc: "book",
+        amount:  -40,  desc: "camera"
+    }]
+
+    console.log(transaction)
+
     return (
         <div className="History">
             <h3>History</h3>
             <hr/>
-            <li>
-                <span>cash</span>
-                <span>500</span>
-        </li>
-        <li>
-                <span>bank</span>
-                <span>-40</span>
-        </li>
-        <li>
-                <span>camera</span>
-                <span>-200</span>
-        </li>
+            {transaction.map((transObj, ind) => {
+                return (
+                <li key={ind}>
+                    <span>{transObj.desc}</span>
+                    <span>{transObj.amount}</span>
+                    
+                </li>
+                )
+            })}
+
+           
         </div>
     )
 }
