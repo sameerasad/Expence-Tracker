efe2fd075e3300c7b7ce90b9c2bc78df66093a46
sameerasad <ssameerkhan525@gmail.com>
2020-10-18 22:01:37 +0500

    react challenge day 5  contextAPI part completed

--- src/TransactionContext.js
+++ src/TransactionContext.js
@@ -1,10 +1,10 @@
 import React,{createContext} from "react";
 const transaction = [{
-    amount:  600,  desc: "Cash",
-    amount: -200,  desc: "book",
-    amount:  -60,  desc: "camera"
+      amount:  600,  desc: "Cash",
+      amount: -200,  desc: "book",
+      amount:  -60,  desc: "camera"
   }]
   
 
 
-export const TransactionContext = createContext(transaction)
+export const TransactionContext = createContext(transaction); 
